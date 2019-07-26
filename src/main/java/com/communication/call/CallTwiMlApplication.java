2cdce48a6be5e84dd7c4f5abbe418083f0ce62ca
Rasool Ansari Shaik <rasoolansaris@virtusa.com>
2019-07-26 08:36:26 +0530

    Updated controller file with get method

--- src/main/java/com/communication/call/CallTwiMlApplication.java
+++ src/main/java/com/communication/call/CallTwiMlApplication.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -23,8 +25,19 @@ public class CallTwiMlApplication {
 	
 	@PostMapping(value="voice", produces= {MediaType.APPLICATION_XML_VALUE})
 	public String getTwiMLData(@RequestBody ObjectNode objNode) {
-		LOGGER.info("initiate Restart");
+		LOGGER.info("post voice method");
 		String message = objNode.get("message").asText();
+		String twiMlMessage = 
+				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?> \n" +
+				"<Response>\n" + 
+				"<Say voice=\"alice\">"+message+"</Say>\n" + 
+				"</Response>";
+		return twiMlMessage;
+	}
+	
+	@GetMapping(value="voice", produces= {MediaType.APPLICATION_XML_VALUE})
+	public String getTwiMLData(@RequestParam String message) {
+		LOGGER.info("get voice method");
 		String twiMlMessage = 
 				"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?> \n" +
 				"<Response>\n" + 
